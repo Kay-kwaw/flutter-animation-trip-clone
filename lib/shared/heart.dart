95eacb69bc09d56164b65b1866976ed3d6b2edc4
kwaw-kumi <kwawkumi9@gmail.com>
2023-02-06 11:06:34 -0800

    tweensequence

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -9,6 +9,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   bool isFav = false;
   late AnimationController _controller;
   late Animation _colorAnimation;
+  late Animation<double> _sizeAnimation;
 
   @override
   void initState() {
@@ -20,6 +21,13 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
 
     _colorAnimation =
         ColorTween(begin: Colors.grey, end: Colors.red).animate(_controller);
+
+    _sizeAnimation = TweenSequence(<TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 50), weight: 50),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 50, end: 30), weight: 50),
+    ]).animate(_controller);
 
     _controller.addListener(() {
       print(_controller.value);
@@ -57,11 +65,11 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
             icon: Icon(
               Icons.favorite,
               color: _colorAnimation.value,
-              size: 30,
+              size: _sizeAnimation.value,
             ),
             onPressed: () {
-              //Tenary operator
-              isFav ? _controller.forward() : _controller.reverse();
+              //Ternary operator
+              isFav ? _controller.reverse() : _controller.forward();
             },
           );
         });
