65982945d15638aeec15f3d7cad8368eeeb31ac1
kwaw-kumi <kwawkumi9@gmail.com>
2023-02-05 21:13:22 -0800

    Color Tween Animation

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,13 +5,32 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _colorAnimation;
+
   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 200),
+        //Like a clock that ticks for animation
+        vsync: this);
+    _controller.forward();
+
+    _colorAnimation =
+        ColorTween(begin: Colors.grey, end: Colors.red).animate(_controller);
+
+    _controller.addListener(() {
+      print(_controller.value);
+    });
+  }
+
   Widget build(BuildContext context) {
     return IconButton(
       icon: const Icon(
         Icons.favorite,
-        color: Color.fromARGB(255, 127, 127, 127),
+        color: Colors.grey,
         size: 30,
       ),
       onPressed: () {},
